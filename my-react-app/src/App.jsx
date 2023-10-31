f4ce47ed2268a9cd17aff67d9fce46b1fa4667ac
Claudia_Alves <claudiascarvalves@gmail.com>
2023-10-31 15:41:34 +0000

    added loading useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,20 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getRandomUsers();
   }, []);
 
   const getRandomUsers = () => {
+    setLoading(true)
     fetch('https://randomuser.me/api/?results=10') 
       .then((response) => response.json())
       .then((data) => {
         const randomUsers = data.results;
         setUsers(randomUsers);
+        setLoading(false)
       })
       .catch((error) => console.error('Error fetching random users: ', error));
   };
@@ -20,13 +23,15 @@ function App() {
   return (
     <div className="App">
       <h1>Users</h1>
-        {users.map((user, index) => (
+        {!loading 
+        ? users.map((user, index) => (
           <div key={index}>
             <h3>{user.name.first} {user.name.last}</h3>
             <p>{user.email}</p>
             <hr />
           </div>
-        ))}
+        ))
+      : "loading"}
     </div>
   );
 }

7820a19aa0f1eb18c2b53af462560639f31c6e24
Claudia_Alves <claudiascarvalves@gmail.com>
2023-10-31 15:33:52 +0000

    fetch request added for randomuser api

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getRandomUsers();
+  }, []);
+
+  const getRandomUsers = () => {
+    fetch('https://randomuser.me/api/?results=10') 
+      .then((response) => response.json())
+      .then((data) => {
+        const randomUsers = data.results;
+        setUsers(randomUsers);
+      })
+      .catch((error) => console.error('Error fetching random users: ', error));
+  };
 
   return (
-    <>
-      <div>
-        Hello world
-      </div>
-    </>
-  )
+    <div className="App">
+      <h1>Users</h1>
+        {users.map((user, index) => (
+          <div key={index}>
+            <h3>{user.name.first} {user.name.last}</h3>
+            <p>{user.email}</p>
+            <hr />
+          </div>
+        ))}
+    </div>
+  );
 }
 
-export default App
+export default App;

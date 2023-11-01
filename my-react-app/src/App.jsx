e2a51b9f2c082287cddddf4202fccf5bc23abcf6
Claudia_Alves <claudiascarvalves@gmail.com>
2023-11-01 18:53:33 +0000

    adding a load users button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -11,15 +11,21 @@ function App() {
 
   const getRandomUsers = () => {
     setLoading(true)
-    fetch('https://randomuser.me/api/?results=10') 
+    fetch('https://randomuser.me/api/?results=5') 
       .then((response) => response.json())
       .then((data) => {
         const randomUsers = data.results;
-        setUsers(randomUsers);
+        setUsers((prevUsers) => [...prevUsers, ...randomUsers]);
         setLoading(false)
       })
       .catch((error) => console.error('Error fetching random users: ', error));
   };
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      getRandomUsers();
+      console.log("More users were loaded")
+  }
 
   return (
     <div className="App">
@@ -29,6 +35,9 @@ function App() {
           <h3>{user.name.first} {user.name.last}</h3>
           <p>{user.email}</p>
           <hr />
+          <form onSubmit={handleSubmit}>
+            <input type="submit" value="load users"></input>
+          </form>
         </div>
       ))
       ) : (
